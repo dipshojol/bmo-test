90eeccdb0ea0e85b23dc7a7afc87e769cbafa218
shojol shake <dip_shojol@yahoo.com>
2020-06-28 19:32:33 -0400

    http request blocking resolved

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,14 +1,13 @@
 import { put, takeEvery, all } from 'redux-saga/effects';
-import { FETCH_RESTAURANT_LIST, updateRestaurantList } from '../actions'
+import { FETCH_RESTAURANT_LIST, updateRestaurantList } from '../actions';
 
 export default function* rootSaga() {
-    yield all([
-        yield takeEvery(FETCH_RESTAURANT_LIST, fetchRestaurant)
-    ])
+  yield all([yield takeEvery(FETCH_RESTAURANT_LIST, fetchRestaurant)]);
 }
 
 function* fetchRestaurant(data) {
-    const { restaurants } = yield fetch(`http://opentable.herokuapp.com/api/restaurants?city=${data.payload}`)
-        .then(response => response.json())
-    yield put(updateRestaurantList(restaurants))
+  const { restaurants } = yield fetch(
+    `https://opentable.herokuapp.com/api/restaurants?city=${data.payload}`
+  ).then(response => response.json());
+  yield put(updateRestaurantList(restaurants));
 }
